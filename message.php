3b021372a91b2b2fa20ddfa991e24be3df60bbb1
Hrilug <3198727736@qq.com>
2023-02-08 23:12:58 +0800

    2023-2-8-1

--- message.php
+++ message.php
@@ -1,5 +1,4 @@
 <?php
-//第一种链接mysql数据库的代码方式；
 $con = mysqli_connect('localhost','root','hrilug');
 if(!$con)
 {
@@ -8,20 +7,16 @@ if(!$con)
 }
 else
 {  
-   //echo "link successfully";
-   // echo $_POST[gonghao];
-
     mysqli_select_db($con,"blog");
     $sql="insert into message(nickname,connect,message) values ('$_POST[nickname]','$_POST[connect]', '$_POST[message]')";
 
-    //if(!mysqli_query($con,"insert into yuangonginf(name,connect,message) values ('hrilug','3198727736@qq.com', 'Hello Word!')"))
     if(!mysqli_query($con, $sql))
     {
         die('Error:'.mysqli_error($con));
     }
     else
     {
-        echo "发送成功，博主很快就会看到哦";
+        echo "发送成功，博主很快就会看到哦（该页面自动跳转）";
         mysqli_close($con);
         header("refresh:2;url=http://www.hrilug.xyz");
     }
