8f2b42f8d4fea001000ab5f518c15250664e3e76
Hrilug <3198727726@qq.com>
2023-07-02 20:02:10 +0800

    2023-7-2-1

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,36 @@
 function detect() {
     if (window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
-        document.getElementById("box").style.width = "80%";
+        document.getElementById("box").style.width = "0";
         return false; // 移动端
     } else {
         return true; // PC端
     }
 }
 
-
 function init() {
     console.log("hello");
+    $("#menu-button").hide();
     if (detect()) {
         console.log("this is a PC");
     }
     else {
         console.log("this is a mobile device");
         //document.getElementById("box").style.display = "hidden";
-        document.getElementById("box").style.display = "none";
-        document.getElementById("menu-button").style.display = "block";
+        //$("#box").width=0;
+        $("#menu-button").show();
     }
 }
 
 window.onload = init();
 
-function menu_show() {
-    document.getElementById("box").style.display = "block";
-
+function menu_show(){
+    $("#box").animate({width:"45%"},200);
 }
 
-function menu_back() {
+function menu_hide(){
     if (detect()) {
-        console.log(device);
     }
-    else {
-        document.getElementById("box").style.display = "none";
+    else{
+    $("#box").animate({width:"0"},200);
     }
 }
